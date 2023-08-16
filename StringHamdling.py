87d7545c09bac24f03c0a6c63a7980424c69c799
Rushikesh <rushikeshakolkar05@gmail.com>
2023-08-16 09:34:32 +0530

    String Handling file added

--- StringHamdling.py
+++ StringHamdling.py
@@ -1,5 +1,32 @@
+#Display String as it is
 blocktext = '''
-EAST WEST
-NORTH SOUTH
+"EAST WEST
+NORTH SOUTH"
 '''
 print(blocktext)
+
+#Counting character in string's
+s1 = "python"
+s2 = "programming"
+print(len(s1))
+print(len(s2))
+
+#lstrim, rstrim, strim
+uname = ' sai'.strip()
+uname1 = ' sai'.lstrip()
+uname2 = 'sai '.rstrip()
+
+if uname == 'sai':
+    print("Hai...",uname)
+else:
+    print("Hello...Guest")
+
+if uname1 == 'sai':
+    print("Hai...",uname)
+else:
+    print("Hello...Guest")
+
+if uname2 == 'sai':
+    print("Hai...",uname)
+else:
+    print("Hello...Guest")
